acc0bab6bc3b257bcdb732aa1fd0b4260f523bc1
WhaleYogurt <101002437+WhaleYogurt@users.noreply.github.com>
2023-02-17 15:41:43 -0500

    Create script.js

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+let counter = 0;
+
+function Change(){
+	document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function Count(){
+	counter += 1;
+	document.getElementById("count").innerHTML = "Count: "+counter; 
+}
+	
