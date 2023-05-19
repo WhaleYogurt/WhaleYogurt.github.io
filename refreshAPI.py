6ea852d36058be9111da44737b0d90247b4a1505
WhaleYogurt <101002437+WhaleYogurt@users.noreply.github.com>
2023-05-19 08:49:42 -0400

    test build

--- refreshAPI.py
+++ refreshAPI.py
@@ -6,17 +6,29 @@ def get_filepaths(directory):
             filepath = os.path.join(root, filename)
             file_paths.append(filepath)
     return file_paths
+def get_filepaths_dict(directory) -> dict:
+    file_paths = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return {'rawData'[file_paths]}
 
 path = r'C:\Users\willy\Desktop\GitHub\WhaleYogurt.github.io'
-full_file_paths = get_filepaths(path)
-toSave = ''
-for file in full_file_paths:
-    toPrint = file.replace('C:\\Users\\willy\\Desktop\\GitHub\\WhaleYogurt.github.io', 'whaleyogurt.github.io')
-    if toPrint != 'whaleyogurt.github.io\index.html':
-        if len(toPrint.split('.git')) == 2 or len(toPrint.split('.idea')) == 2:
-            toSave += '                "'+toPrint.replace(path[2], '/')+'",\n'
-    else:
-        toSave += '{\n    "rawData": ["whaleyogurt.github.io",\n'
-toSave = toSave[0:len(toSave)-2] +'\n    ]\n}'
-with open('API/data.json', 'w') as fh:
-    fh.write(toSave)
+full_file_paths = get_filepaths_dict(path)
+print(full_file_paths)
+
+if faulthandler == '':
+    path = r'C:\Users\willy\Desktop\GitHub\WhaleYogurt.github.io'
+    full_file_paths = get_filepaths(path)
+    toSave = ''
+    for file in full_file_paths:
+        toPrint = file.replace('C:\\Users\\willy\\Desktop\\GitHub\\WhaleYogurt.github.io', 'whaleyogurt.github.io')
+        if toPrint != 'whaleyogurt.github.io\index.html':
+            if len(toPrint.split('.git')) == 2 or len(toPrint.split('.idea')) == 2:
+                toSave += '                "'+toPrint.replace(path[2], '/')+'",\n'
+        else:
+            toSave += '{\n    "rawData": ["whaleyogurt.github.io",\n'
+    toSave = toSave[0:len(toSave)-2] +'\n    ]\n}'
+    with open('API/data.json', 'w') as fh:
+        fh.write(toSave)
